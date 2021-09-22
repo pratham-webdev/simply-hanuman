f9fd6d6fbb39470de2ae42bb8a74a77c27174085
Pratham <pratham.webdev@gmail.com>
2021-09-22 21:16:32 +0530

    footer changes

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,28 @@
-const footerHtml = `
-<div class="container">
-<div class="py-3">
-<div class="d-flex align-items-center justify-content-between">
-<span>This is an open source, non-profit & community project. Please reach out on Github/Contact for adding information or corrections. Citiations for text to Wikipedia.</span>
-<div>
-<a href="https://github.com/pratham-webdev/simply-hanuman" class="me-3" target="_blank">Github</a>
-<a href="mailto:pratham.webdev@gmail.com" class="me-3">Contact</a>
-</div>
-</div>
-</div>`;
+const footerHtml = 
+  `<div class="container">
+    <div class="py-3">
+      <div class="row">
+        <div class="col-12 col-lg-9">
+          <span>
+            This is an open source, non-profit & community project. Please reach
+            out on Github/Contact for adding information or corrections.
+            Citiations for text to Wikipedia.
+          </span>
+        </div>
+        <div class="col-12 col-lg-3 d-flex justify-content-end">
+          <a
+            href="https://github.com/pratham-webdev/simply-hanuman"
+            class="me-3"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a href="mailto:pratham.webdev@gmail.com" class="me-3">
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>`;
 
 $(`#footer`).append(footerHtml);
